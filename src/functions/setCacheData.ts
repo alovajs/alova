a253e9d1c638c5402c3c41f63ba4208122eb0e92
hu <huzhen555@qq.com>
2022-10-19 15:49:53 +0800

    fix: hit persistent data in function setCacheData

--- src/functions/setCacheData.ts
+++ src/functions/setCacheData.ts
@@ -1,12 +1,12 @@
 import { MethodMatcher } from '../../typings';
 import Method from '../Method';
 import { getMethodSnapshot, getResponseCache, keyFilter, setResponseCache } from '../storage/responseCache';
-import { persistResponse } from '../storage/responseStorage';
+import { getPersistentResponse, persistResponse } from '../storage/responseStorage';
 import { getLocalCacheConfigParam, instanceOf, isFn, key } from '../utils/helper';
-import { falseValue, forEach, getContext } from '../utils/variables';
+import { falseValue, forEach, getContext, undefinedValue } from '../utils/variables';
 
 /**
- * 手动设置缓存响应数据
+ * 手动设置缓存响应数据，如果对应的methodInstance设置了持久化存储，则还会去检出持久化存储中的缓存
  * @param methodInstance 请求方法对象
  * @param data 缓存数据
  */
@@ -23,7 +23,10 @@ export default function setCacheData<R = any, S = any, E = any, T = any, RC = an
 		const methodKey = key(methodInstance);
 		let data: any = dataOrUpater;
 		if (isFn(dataOrUpater)) {
-			data = dataOrUpater(getResponseCache(id, methodKey));
+			data = dataOrUpater(
+				getResponseCache(id, methodKey) ||
+					(toStorage ? getPersistentResponse(id, methodKey, storage, tag) : undefinedValue)
+			);
 			if (data === falseValue) {
 				return;
 			}
