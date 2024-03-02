9845dc501a6c33fe896a1980ba7d3c797a9df64c
JOU Amjs <huzhen555@qq.com>
2024-03-02 14:06:55 +0800

    fix: default cache logger is compatible with server env and env that without console.groupCollapsed
    fix #285

--- src/predefine/defaultCacheLogger.ts
+++ src/predefine/defaultCacheLogger.ts
@@ -1,3 +1,4 @@
+import { STORAGE_RESTORE, isSSR, len } from '@/utils/variables';
 import { CacheMode } from '~/typings';
 import { Method } from '..';
 
@@ -6,13 +7,29 @@ const titleStyle = 'color: black; font-size: 12px; font-weight: bolder';
  * 默认cacheLogger函数
  */
 export default (response: any, methodInstance: Method, cacheMode: CacheMode, tag: string | number | undefined) => {
-  const cole = console;
-  cole.groupCollapsed('%cHitCache', 'padding: 2px 6px; background: #c4fcd3; color: #53b56d;', methodInstance.url);
-  cole.log('%c[Mode]', titleStyle, cacheMode);
-  if (cacheMode === 'restore') {
-    cole.log('%c[Tag]', titleStyle, tag);
+  const cole = console,
+    log = (...args: any[]) => console.log(...args),
+    url = methodInstance.url,
+    isRestoreMode = cacheMode === STORAGE_RESTORE,
+    hdStyle = '\x1B[42m%s\x1B[49m',
+    labelStyle = '\x1B[32m%s\x1B[39m',
+    startSep = ` [HitCache]${url} `,
+    endSepFn = () => Array(len(startSep) + 1).join('^');
+  if (isSSR) {
+    log(hdStyle, startSep);
+    log(labelStyle, ' Cache ', response);
+    log(labelStyle, ' Mode  ', cacheMode);
+    isRestoreMode && log(labelStyle, ' Tag   ', tag);
+    log(labelStyle, endSepFn());
+  } else {
+    cole.groupCollapsed
+      ? cole.groupCollapsed('%cHitCache', 'padding: 2px 6px; background: #c4fcd3; color: #53b56d;', url)
+      : log(hdStyle, startSep);
+
+    log('%c[Cache]', titleStyle, response);
+    log('%c[Mode]', titleStyle, cacheMode);
+    isRestoreMode && log('%c[Tag]', titleStyle, tag);
+    log('%c[Method]', titleStyle, methodInstance);
+    cole.groupEnd ? cole.groupEnd() : log(labelStyle, endSepFn());
   }
-  cole.log('%c[Method]', titleStyle, methodInstance);
-  cole.log('%c[Cache]', titleStyle, response);
-  cole.groupEnd();
 };
