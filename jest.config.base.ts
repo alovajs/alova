26b96ceb1430acb298ed09c9021930bc49481845
胡镇 <huzhen555@qq.com>
2024-05-15 11:20:03 +0800

    test: update ignore coverage path patterns

--- jest.config.base.ts
+++ jest.config.base.ts
@@ -32,7 +32,7 @@ const config: Config = {
   coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: ['\\\\node_modules\\\\', '/node_modules/', '../../internal/', 'test/components'],
+  coveragePathIgnorePatterns: ['\\\\node_modules\\\\', '/node_modules/', '../../internal/', 'test/**/components', 'test/utils'],
 
   // Indicates which provider should be used to instrument code for coverage
   // coverageProvider: 'v8',
