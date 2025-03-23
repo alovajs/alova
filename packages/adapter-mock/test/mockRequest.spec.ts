b3be8520adc3f72b6dce3d200032bf20d0d9311e
Nakus0426 <820774459@qq.com>
2025-03-23 20:46:13 +0800

    test(mock): 补充test

--- packages/adapter-mock/test/mockRequest.spec.ts
+++ packages/adapter-mock/test/mockRequest.spec.ts
@@ -474,4 +474,23 @@ describe('mock request', () => {
       .send();
     expect(data).toStrictEqual({ id: 1 });
   });
+
+  test('should return string data when post request data type is string', async () => {
+    const mocks = defineMock({
+      '[POST]/detail': ({ data }) => data
+    });
+    // Mock Data Request Adapter
+    const mockRequestAdapter = createAlovaMockAdapter([mocks], {
+      onMockResponse({ body }) {
+        return {
+          response: body,
+          headers: {}
+        };
+      }
+    });
+    const data = await createAlova({ baseURL: 'http://xxx', requestAdapter: mockRequestAdapter })
+      .Post('/detail', 'hello')
+      .send();
+    expect(data).toBe('hello');
+  });
 });
